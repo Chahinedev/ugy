afc4947e4b8abbe3075b06cbe2fd1c0e55f06268
chahine <chahine.ouasti@gmail.com>
2024-06-19 12:11:36 +0200

    changes

---
+++ ugy/Preview Content/SwiftUIView.swift
@@ -0,0 +1,8 @@
+//
+//  SwiftUIView.swift
+//  ugy
+//
+//  Created by Chahine Ouasti on 19/06/2024.
+//
+
+import Foundation
---
+++ ugy/Preview Content/background.swift
@@ -0,0 +1,8 @@
+//
+//  background.swift
+//  ugy
+//
+//  Created by Chahine Ouasti on 19/06/2024.
+//
+
+import Foundation
